cf3cc95986fe4410cac0acf83d631a80348f8126
bolez <166941785+bolez@users.noreply.github.com>
2025-06-23 14:03:56 +0530

    Update list_product.sql

--- {{cookiecutter.product_name}}/{{cookiecutter.domain_name}}/macros/list_product.sql
+++ {{cookiecutter.product_name}}/{{cookiecutter.domain_name}}/macros/list_product.sql
@@ -1,57 +1,46 @@
-{% raw %}
 
 {% macro list_product(object_schema, object_name, share_name) %}
-    -- Grant SELECT permission on the table to the share
-    GRANT SELECT ON TABLE {{ object_name }} TO SHARE {{ share_name }};
+    GRANT SELECT ON table  {{object_name}} TO SHARE {{ share_name }};
 
-    -- Check if the listing already exists
-    {% set existing_listings_query %}
-        SHOW LISTINGS LIKE '{{ share_name }}';
-    {% endset %}
-    {% set existing_listings_result = run_query(existing_listings_query) %}
-   
-        {% if not existing_listings_result %}
-        {% set object_name_str = object_name.render() %}
-        {% set table_name = object_name_str.split('.')[-1] %}
-
-        -- Define the SQL for creating the organization listing
-
-        CREATE ORGANIZATION LISTING {{ share_name }}
-        SHARE {{ share_name }} AS
-        $$
-        title : '{{ share_name }}'
-        description: "Created using dbt and cookicutter {{ share_name }}"
-        organization_profile: INTERNAL
-        organization_targets:
-          discovery:
-              - all_accounts : true
+  {% set object_name_str = object_name.render() %}
+  {% set table_name = object_name_str.split('.')[-1] %}
+    {% set sql %}
+    -- GRANT SELECT ON TABLE DEVORGDB5.PUBLIC.TUTORIAL_TABLE TO SHARE ORG_SHARE5;
+    --   GRANT USAGE ON SCHEMA {{ target.database }}.{{ view_schema }} TO SHARE {{ share_name }};
+      GRANT SELECT ON TABLE {{ object_name }} TO SHARE {{ share_name }};
+      CREATE ORGANIZATION LISTING {{share_name}}
+      SHARE {{share_name}} AS
+      $$
+      title : '{{share_name}}'
+      description: "Created using dbt and cookicutter {{share_name}}"
+      organization_profile: INTERNAL
+      organization_targets:
           access:
-          - account : 'PQ57060'
-            roles: ['ACCOUNTADMIN', {{ var('engg_role')}]
-        locations:
-          access_regions:
-          - name: "ALL"
-        auto_fulfillment:
-          refresh_type: SUB_DATABASE
-          refresh_schedule: '10 MINUTE'
-        support_contact: "support@somedomain.com"
-        approver_contact: "approver@somedomain.com"
-        data_dictionary:
-          featured:
-            database: "{{ var('domain_database') }}"
-            objects:
-              - name: "{{ table_name }}"
-                schema: "{{ object_schema }}"
-                domain: "TABLE"
-        usage_examples:
-          - title: "Return all {{ object_name }} for the {{ object_name }}"
-            description: "Example of how to select {{ object_name }} information"
-            query: "select * from {{ object_name }}"
-        resources:
-          documentation: https://www.example.com/documentation/
-        $$;
+          - all_accounts : false 
+      locations:
+        access_regions:
+        - name: "ALL"
+      support_contact: "support@somedomain.com"
+      approver_contact: "approver@somedomain.com"
+      data_dictionary:
+        featured:
+          database: "{{var('domain_database')}}"
+          objects:
+            - name: "{{table_name}}"
+              schema: "{{object_schema}}"
+              domain: "TABLE"
+      usage_examples:
+        - title: "Return all {{object_name}} for the {{object_name}}"
+          description: "Example of how to select {{object_name}} information"
+          query: "select * from {{object_name}}"
+      resources:
+        documentation: https://www.example.com/documentation/
+      $$;
 
-  {% endif %}
-    
+
+    {% endset %}
+
+    {% set table = run_query(sql) %}
+
+
 {% endmacro %}
-{% endraw %}
